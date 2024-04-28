9b9e138086a4d3d6cbdb505c0f44d0f0cbcc9a73
Satyam Garg <satyamgarg1308@gmail.com>
2024-04-28 21:21:23 +0530

    backend url update

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -139,7 +139,7 @@ const totalPrice = data.reduce((prev,curr)=> prev + (curr.quantity * curr?.produ
             products : data
         }
 
-        const response = await fetch("http://localhost:8080/api/checkout",{
+        const response = await fetch("https://ezshop-v56r.onrender.com/api/checkout",{
             method:"POST",
             headers:headers,
             body:JSON.stringify(body)
