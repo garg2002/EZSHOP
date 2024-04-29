d701db7c617dc2e938e6950fcec5c69b52bda211
Satyam Garg <satyamgarg1308@gmail.com>
2024-04-30 00:54:26 +0530

    secure value update

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -35,7 +35,7 @@ async function userSignInController(req,res){
 
         const tokenOption = {
             httpOnly : true,
-            secure : true
+            secure : false
         }
 
         res.cookie("token",token,tokenOption).status(200).json({
