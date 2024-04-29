0200493a851415a04e2429927e1d38b641c64f09
Satyam Garg <satyamgarg1308@gmail.com>
2024-04-29 22:16:31 +0530

    update

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 async function authToken(req,res,next){
     try{
-        const {token} = req.cookies?.token;
+        const token = req.cookies?.token;
 
         if(!token){
             return res.status(200).json({
