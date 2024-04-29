682be8584c218309a649a3dd91094408d7000c76
Satyam Garg <satyamgarg1308@gmail.com>
2024-04-29 22:01:18 +0530

    updated

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -1,59 +1,40 @@
 const jwt = require('jsonwebtoken')
-const AppError = require('./AppError')
 
-async function authToken(req,_res,next){
-    // try{
-    //     const {token} = req.cookies;
+async function authToken(req,res,next){
+    try{
+        const {token} = req.cookies?.token;
 
-    //     if(!token){
-    //         return res.status(200).json({
-    //             message : "Please Login...!",
-    //             error : true,
-    //             success : false
-    //         })
-    //     }
+        if(!token){
+            return res.status(200).json({
+                message : "Please Login...!",
+                error : true,
+                success : false
+            })
+        }
 
-    //     const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+        const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+            
+            
+            if(err){
+                console.log("error aa gai re", err)
+            }
 
-    //        console.log("decoded:", decoded);
-    //         req.userId = decoded.id;
+            req.userId = decoded?._id
 
-    //         next();
-      
-
+            next()
+        });
+        console.log("decode", decode);
        
 
 
-    // }catch(err){
-    //     res.status(400).json({
-    //         message : err.message || err,
-    //         data : [],
-    //         error : true,
-    //         success : false
-    //     })
-    // }
-
-    const token = req.cookies;
-
-    if (!token) {
-        return next(new AppError("Unauthorized, please login to continue", 401));
-      }
-    
-      // Decoding the token using jwt package verify method
-      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-      
-    
-      // If no decode send the message unauthorized
-      if (!decoded) {
-        return next(new AppError("Unauthorized, please login to continue", 401));
-      }
-    console.log("decoded:", decoded);
-      // If all good store the id in req object, here we are modifying the request object and adding a custom field user in it
-      req.userId = decoded.id;
-    
-      // Do not forget to call the next other wise the flow of execution will not be passed further
-      next();
-    
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            data : [],
+            error : true,
+            success : false
+        })
+    }
 }
 
 
