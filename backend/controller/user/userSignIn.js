bb3b4f7b53660d06c8f67cd21c3c3b7d6f841e42
Satyam Garg <satyamgarg1308@gmail.com>
2024-04-30 01:10:24 +0530

    updated

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -33,18 +33,21 @@ async function userSignInController(req,res){
         console.log(tokenData);
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
-        const tokenOption = {
-            httpOnly : true,
-            secure : true,
-            sameSite:' None',
-        }
+      
 
-        res.cookie("token",token,tokenOption).status(200).json({
+        res.cookie("token",token).status(200).json({
             message : "Login successfully",
             data : token,
             success : true,
             error : false
         })
+
+        res.cookie("token",token,{
+            httpOnly :true,
+            sameSite: 'None',
+            secure: true,
+        }).send()
+    
 
 
 
